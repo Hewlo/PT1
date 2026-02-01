37887926498ef5eb3674a0f87023aa36f7a5dd1d
Hewlo <hamstersmart1@hotmail.com>
2026-02-01 14:12:52 +0800

    Adapted for Windows

--- ManageFile.py
+++ ManageFile.py
@@ -11,8 +11,8 @@ logger = logging.getLogger()
 logger.setLevel('INFO')
 
 project_directory = os.getcwd()
-resource_directory = os.getcwd() + '/resource'
-txt_file_directory = os.getcwd() + '/resource/student_submission.txt'
+resource_directory = os.getcwd() + r'\\resource'
+txt_file_directory = os.getcwd() + r'\\resource\student_submission.txt'
 
 #TASK A
 with open(txt_file_directory, 'r') as txt_contents:
@@ -23,18 +23,18 @@ with open(txt_file_directory, 'r') as txt_contents:
         project, owner = lines.split(':')
         file_name, file_extension = os.path.splitext(project)
         project_name = file_name + '_' + owner + file_extension
-        target_project_name = resource_directory + '/' + project
+        target_project_name = resource_directory + r'\\' + project
         try:
-            os.rename(target_project_name, resource_directory + '/' + project_name)
+            os.rename(target_project_name, resource_directory + r'\\' + project_name)
             logger.info('Rename file: {} rename to {}'.format(target_project_name, project_name))
         except FileNotFoundError:
             logger.info('Skipped file: {}'.format(project_name))
 
 #TASK B
 #I don't know how I come up with these.
-split_resource_path = resource_directory.split('/')
+split_resource_path = resource_directory.split(r'\\')
 copy_target_name = split_resource_path[split_resource_path.index(max(split_resource_path))]
-backup_folder_directory = os.getcwd() + '/' + strftime('%Y_%m_%d_{}'.format(copy_target_name))
+backup_folder_directory = os.getcwd() + r'\\' + strftime('%Y_%m_%d_{}'.format(copy_target_name))
 if not os.path.exists(backup_folder_directory):
     shutil.copytree(resource_directory, backup_folder_directory)
     logger.info('Backing up folder: {} created'.format(backup_folder_directory))
@@ -43,7 +43,7 @@ if not os.path.exists(backup_folder_directory):
 zip_file = '{}_{}.zip'.format(strftime('%Y_%m_%d_%H_%M_%S'), copy_target_name)
 with zipfile.ZipFile(zip_file,'w') as archive:
     for files in os.listdir(resource_directory):
-        file_path = backup_folder_directory + '/' + files
+        file_path = backup_folder_directory + r'\\' + files
         archive.write(file_path)
     logger.info('Compressing file {}'.format(archive.filename))
     shutil.rmtree(backup_folder_directory)
--- reset.py
+++ reset.py
@@ -6,7 +6,7 @@ for items in os.listdir(os.getcwd()):
     item_path = os.getcwd() + r'/' + items
     file, file_extension = os.path.splitext(items)
     if items == 'resource':
-        backup_path = os.getcwd() + r'/' + 'resource (1)'
+        backup_path = os.getcwd() + r'\\' + 'resource (1)'
         shutil.rmtree(item_path)
         shutil.copytree(backup_path, item_path)
     if file_extension == '.txt' or file_extension == '.zip':
