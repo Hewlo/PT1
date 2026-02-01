0ae16ef710f353b77423b040729736f603e3afe4
hewlo <hamstersmart1@hotmail.com>
2026-02-01 11:49:47 +0800

    Improved readability

--- ManageFile.py
+++ ManageFile.py
@@ -11,9 +11,8 @@ logger = logging.getLogger()
 logger.setLevel('INFO')
 
 project_directory = os.getcwd()
-resource_directory = os.getcwd() + r'/resource'
-txt_file_directory = os.getcwd() + r'/resource/student_submission.txt'
-# txt_contents = open(txt_file_directory, 'r')
+resource_directory = os.getcwd() + '/resource'
+txt_file_directory = os.getcwd() + '/resource/student_submission.txt'
 
 #TASK A
 with open(txt_file_directory, 'r') as txt_contents:
@@ -21,15 +20,15 @@ with open(txt_file_directory, 'r') as txt_contents:
         lines = lines.rstrip('\n')
         if lines == '' or ':' not in lines:
             continue
-        project_name, project_owner = lines.split(':')
-        file_name, file_extension = os.path.splitext(project_name)
-        project_full_name = file_name + '_' + project_owner + file_extension
-        target_project_name = resource_directory + r'/{}'.format(project_name)
+        project, owner = lines.split(':')
+        file_name, file_extension = os.path.splitext(project)
+        project_name = file_name + '_' + owner + file_extension
+        target_project_name = resource_directory + '/' + project
         try:
-            os.rename(target_project_name, resource_directory + r'/{}'.format(project_full_name))
-            logger.info('Rename file: {} rename to {}'.format(target_project_name, project_full_name))
+            os.rename(target_project_name, resource_directory + '/' + project_name)
+            logger.info('Rename file: {} rename to {}'.format(target_project_name, project_name))
         except FileNotFoundError:
-            logger.info('Skipped file: {}'.format( project_full_name))
+            logger.info('Skipped file: {}'.format(project_name))
 
 #TASK B
 #I don't know how I come up with these.
@@ -41,9 +40,10 @@ if not os.path.exists(backup_folder_directory):
     logger.info('Backing up folder: {} created'.format(backup_folder_directory))
 
 #TASK C
-with zipfile.ZipFile('{}_{}.zip'.format(strftime('%Y_%m_%d_%H_%M_%S'), copy_target_name),'w') as archive:
+zip_file = '{}_{}.zip'.format(strftime('%Y_%m_%d_%H_%M_%S'), copy_target_name)
+with zipfile.ZipFile(zip_file,'w') as archive:
     for files in os.listdir(resource_directory):
-        file_path = backup_folder_directory + r'/{}'.format(files)
+        file_path = backup_folder_directory + '/' + files
         archive.write(file_path)
     logger.info('Compressing file {}'.format(archive.filename))
     shutil.rmtree(backup_folder_directory)
